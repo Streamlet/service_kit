e9566e89105b086d0e024149ecc63e865459ec93
柯津津（溪流） <jinjin.kejj@alibaba-inc.com>
2022-04-05 11:22:43 +0800

    update

--- build_sample.sh
+++ build_sample.sh
@@ -10,9 +10,9 @@ cmake -DGRPC_INSTALL_DIR=$GRPC_INSTALL_DIR \
       -DBOOST_INSTALL_DIR=$BOOST_INSTALL_DIR \
       -DSERVICE_KIT_INSTALL_DIR=$SERVICE_KIT_INSTALL_DIR \
       -DCMAKE_INSTALL_PREFIX=$DIR/sample \
-      -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% \
+      -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
       $CMAKE_EXTRA_FLAGS \
       ..
-cmake --build . --parallel %CORE_COUNT%
+cmake --build . --parallel $CORE_COUNT
 cmake --install .
 popd
