7f7012a7e65c679006fffc8d16404c6b5021bdd4
streamlet <streamlet@outlook.com>
2022-04-10 04:15:56 +0800

    use find_package to find boost

--- build_sample.sh
+++ build_sample.sh
@@ -7,7 +7,7 @@ source $DIR/build_config.sh
 mkdir -p $DIR/sample/helloworld/_build
 pushd $DIR/sample/helloworld/_build
 cmake -DGRPC_INSTALL_DIR=$GRPC_INSTALL_DIR \
-      -DBOOST_INSTALL_DIR=$BOOST_INSTALL_DIR \
+      -DTHIRD_PARTY_INSTALL_DIR=$THIRD_PARTY_INSTALL_DIR \
       -DSERVICE_KIT_INSTALL_DIR=$SERVICE_KIT_INSTALL_DIR \
       -DCMAKE_INSTALL_PREFIX=$DIR/sample \
       -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
